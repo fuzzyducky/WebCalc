464cd8a3a52de84a95221f73b343c427f578789f
Aaron Morrison <armorrison@gmail.com>
2017-06-09 14:23:25 -0400

    adding mongo

--- test_webcalc.py
+++ test_webcalc.py
@@ -2,11 +2,23 @@ import pytest
 from expecter import expect
 
 from webcalc import app
+from webcalc import mongo
 
 
 @pytest.fixture
 def client():
     return app.test_client()
+
+@pytest.fixture
+def pattern():
+    with app.app_context():
+        mongo.db.operations.drop()
+        mongo.db.operations.insert(
+            dict(
+                name="x",
+                pattern="{{ a * b }}"
+            )
+        )
 
 def describe_index():
 
@@ -19,3 +31,8 @@ def describe_calc():
     def when_plus(client):
         response = client.get('/4/+/5')
         expect(response.data).contains(b"9")
+
+    def from_db(client, pattern):
+        response = client.get('/4/x/5')
+
+        expect(response.data).contains(b"20")
