0df6f39e057c2ce9ab6d9b186513a844d6320e69
Aaron Morrison <armorrison@gmail.com>
2017-06-09 13:27:30 -0400

    Added a new route and test

--- webcalc.py
+++ webcalc.py
@@ -8,6 +8,9 @@ app = Flask(__name__)
 def index():
     return "Hello, Aaron!"
 
+@app.route('/<int:a>/<op>/<int:b>')
+def calc(a, op, b):
+    return f"Result: {a} {op} {b} = {a + b}"
 
 if __name__ == '__main__':
     app.run(debug=True)
